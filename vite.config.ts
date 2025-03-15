6f21dec7a21b3a93b05726b7bfc2a5fe72ae1c52
Sadra <sadra.shameli1@gmail.com>
2025-03-15 18:10:06 +0100

    chore: code adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -46,20 +46,22 @@ export default defineConfig(function () {
         server: {
             host: 'minomarktnl.test',
             cors: true,
-            https: {
-                key: fs.readFileSync(
-                    path.resolve(
-                        __dirname,
-                        '.wordpress/certs/minomarktnl.test.key',
-                    ),
-                ),
-                cert: fs.readFileSync(
-                    path.resolve(
-                        __dirname,
-                        '.wordpress/certs/minomarktnl.test.crt',
-                    ),
-                ),
-            },
+            https: isProd
+                ? undefined
+                : {
+                      key: fs.readFileSync(
+                          path.resolve(
+                              __dirname,
+                              '.wordpress/certs/minomarktnl.test.key',
+                          ),
+                      ),
+                      cert: fs.readFileSync(
+                          path.resolve(
+                              __dirname,
+                              '.wordpress/certs/minomarktnl.test.crt',
+                          ),
+                      ),
+                  },
         },
     };
 });
